15b62b72aa16cdeef401b51622e96de5d244651d
Abdallah Ahmed <Abdallah.Ahmed@SOfCO.Org>
2026-01-17 18:21:30 +0200

    chore: force english language and update deployment

--- src/i18n.js
+++ src/i18n.js
@@ -20,6 +20,7 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
+        lng: 'en', // <--- Force English language
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false // react already safes from xss
